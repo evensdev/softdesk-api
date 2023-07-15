a13d43b6d3cd74690bd53b3def33e7a179d95bb5
S. Evens JOSEPH <84663627+evensdev@users.noreply.github.com>
2023-07-15 15:18:46 +0200

    SFDSK-15 User model

--- quickstart/models.py
+++ quickstart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    consent_choice = models.BooleanField(default=False)
+
+
